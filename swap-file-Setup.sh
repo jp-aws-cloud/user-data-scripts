565072f6d310a7e799eb366627ac5448cef0f513
Harsh Rajotya <114584351+joy00900@users.noreply.github.com>
2025-09-05 11:27:25 +0530

    Update swap-file-Setup.sh
    workind and updated

--- swap-file-Setup.sh
+++ swap-file-Setup.sh
@@ -1,15 +1,17 @@
-#!/bin/bash # Swap file setup script    
+#!/bin/bash
+# Swap file setup script    
 # For low-memory EC2 (t2.micro, t3.micro)
-set -euo pipefail # safer bash scripting    
 
-SWAP_SIZE=2G # Define swap size 
+set -euo pipefail
 
-fallocate -l $SWAP_SIZE /swapfile # Create swap file    
-chmod 600 /swapfile # Secure the swap file  
-mkswap /swapfile # Set up swap area 
-swapon /swapfile  # Activate swap   
-echo "/swapfile none swap sw 0 0" >> /etc/fstab  # Ensure swap is enabled on boot   
+SWAP_SIZE=2G
 
-swapon --show # Verify swap is active   
-free -h   # Display memory and swap usage
-echo "Swap file of size $SWAP_SIZE created and activated."  # Completion message
+fallocate -l $SWAP_SIZE /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo "/swapfile none swap sw 0 0" >> /etc/fstab
+
+swapon --show
+free -h
+echo "Swap file of size $SWAP_SIZE created and activated."
